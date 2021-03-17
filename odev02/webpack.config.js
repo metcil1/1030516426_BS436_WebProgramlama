ea3fe583cadce085ff19819bd8a8f7721ab36653
metcil1 <mustafaetcil1@gmail.com>
2021-03-17 19:18:58 +0300

    Odev02_Son

---
+++ odev02/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    entry : './src/kod.js',
+    output : {
+        filename:"bundle.js",
+        path: path.resolve(__dirname,'public'),
+        libraryTarget : "var",
+        library:"Resimbul",
+
+    },
+    devServer:{
+        contentBase : './public',
+        injectClient : false
+    },
+    optimization:{
+        minimize:true,
+        minimizer:[
+            new TerserPlugin({
+                extractComments:false
+            })
+        ]
+    }
+}
